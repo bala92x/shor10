6266e4767fa685400fca37b931ecc399bd6a1457
bala92x <bala92x@gmail.com>
2019-03-20 18:16:35 +0100

    Set up adding links

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 
 import { onEnterPrivatePage } from '../utils/auth';
+import { Links } from '../api/links';
 
 export default class Link extends React.Component {
     onLogout = () => {
         Accounts.logout();
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const url = this.refs.url.value.trim();
+
+        if (url) {
+            Links.insert({ url });
+            this.refs.url.value = '';
+        }
     }
 
     componentWillMount() {
@@ -18,6 +30,11 @@ export default class Link extends React.Component {
                 <h1>Links</h1>
     
                 <button onClick={this.onLogout}>Logout</button>
+                <p>Add Link</p>
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" ref="url" placeholder="URL" />
+                    <button>Add Link</button>
+                </form>
             </div>
         );
     }
