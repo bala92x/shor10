ee7fcb169b8d9c29b41a8a4c7f2a98cb4816642b
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-22 14:54:36 +0100

    Change copy to clipboard button message on click

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            justCopied: false
+        };
+    }
+    
     componentDidMount() {
         this.clipboard = new Clipboard(this.refs.copy);
 
         this.clipboard
             .on('success', () => {
-                //this.refs.copy.text = 'Copied';
+                this.setState({ justCopied: true });
+                setTimeout(() => {
+                    this.setState({justCopied: false});
+                }, 1000);
             })
             .on('error', () => {
                 alert('Unable to copy. Please copy the link manually.');
@@ -24,7 +34,9 @@ export default class LinksListItem extends React.Component {
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
-                <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+                <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+                    { this.state.justCopied ? 'Copied' : 'Copy' }
+                </button>
             </div>
         );
     }
