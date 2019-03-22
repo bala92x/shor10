4a53aa126d1cf6be8a68adac9ecc24a5267a9bff
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-22 15:12:46 +0100

    Add functioning hide link button

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
@@ -29,13 +30,21 @@ export default class LinksListItem extends React.Component {
         this.clipboard.destroy();
     }
 
+    onHideClick = () => {
+        Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <p>{this.props.visible.toString()}</p>
                 <button ref="copy" data-clipboard-text={this.props.shortUrl}>
                     { this.state.justCopied ? 'Copied' : 'Copy' }
+                </button>
+                <button onClick={this.onHideClick}>
+                    { this.props.visible ? 'Hide' : 'Unhide' }
                 </button>
             </div>
         );
@@ -47,4 +56,5 @@ LinksListItem.propTypes = {
     url: PropTypes.string.isRequired,
     userId: PropTypes.string.isRequired,
     shortUrl: PropTypes.string.isRequired,
+    visible: PropTypes.bool.isRequired
 }
