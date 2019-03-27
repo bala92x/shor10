5e7e6778a8d2f9714a17866cb33073dd95421bf1
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-27 15:17:18 +0100

    Add boxed view styles

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -33,18 +33,21 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Login to Shor10</h1>
+            <div className="boxed-view">
+                <div className="boxed-view__box">
+                    <h1>Login to Shor10</h1>
 
-                {this.state.error ? <p>{this.state.error}</p> : undefined}
+                    {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-                <form onSubmit={this.onSubmit} noValidate>
-                    <input type="email" name="email" ref="email" placeholder="Email" autoComplete="true" />
-                    <input type="password" name="password" ref="password" placeholder="Password" autoComplete="true" />
-                    <button>Login</button>
-                </form>
+                    <form onSubmit={this.onSubmit} noValidate>
+                        <input type="email" name="email" ref="email" placeholder="Email" autoComplete="true" />
+                        <input type="password" name="password" ref="password" placeholder="Password" autoComplete="true" />
+                        <button>Login</button>
+                    </form>
 
-                <Link to="/signup">Join</Link>
+                    <Link to="/signup">Join</Link>
+                </div>
+
             </div>
         );
     }
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -37,18 +37,20 @@ export default class Signup extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Join to Shor10</h1>
+            <div className="boxed-view">
+                <div className="boxed-view__box">
+                    <h1>Join to Shor10</h1>
 
-                {this.state.error ? <p>{this.state.error}</p> : undefined}
+                    {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-                <form onSubmit={this.onSubmit} noValidate>
-                    <input type="email" name="email" ref="email" placeholder="Email" />
-                    <input type="password" name="password" ref="password" placeholder="Password" />
-                    <button>Create Account</button>
-                </form>
+                    <form onSubmit={this.onSubmit} noValidate>
+                        <input type="email" name="email" ref="email" placeholder="Email" />
+                        <input type="password" name="password" ref="password" placeholder="Password" />
+                        <button>Create Account</button>
+                    </form>
 
-                <Link to="/">Already have an account?</Link>
+                    <Link to="/">Already have an account?</Link>
+                </div>
             </div>
         );
     }
