04366c92f90e2fa124db0eb156f4971e8595238a
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-27 12:53:19 +0100

    Add visit tracking

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -40,6 +40,7 @@ export default class LinksListItem extends React.Component {
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
                 <p>{this.props.visible.toString()}</p>
+                <p>{this.props.visitedCount} - {this.props.lastVisitedAt}</p>
                 <button ref="copy" data-clipboard-text={this.props.shortUrl}>
                     { this.state.justCopied ? 'Copied' : 'Copy' }
                 </button>
@@ -56,5 +57,7 @@ LinksListItem.propTypes = {
     url: PropTypes.string.isRequired,
     userId: PropTypes.string.isRequired,
     shortUrl: PropTypes.string.isRequired,
-    visible: PropTypes.bool.isRequired
+    visible: PropTypes.bool.isRequired,
+    visitedCount: PropTypes.number.isRequired,
+    lastVisitedAt: PropTypes.number
 }
